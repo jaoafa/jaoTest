152c1ac313bd037f117a51fa88398a2b6255932b
Tomachi [ICHIGO] <lego@aol.jp>
2021-12-04 02:17:41 +0900

    feat: 1.18

--- src/main/java/com/jaoafa/jaotest/Event_JoinVerifiedCheck.java
+++ src/main/java/com/jaoafa/jaotest/Event_JoinVerifiedCheck.java
@@ -1,5 +1,8 @@
 package com.jaoafa.jaotest;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.JoinConfiguration;
+import net.kyori.adventure.text.format.NamedTextColor;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -13,10 +16,7 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 import java.time.ZoneId;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.TimeZone;
-import java.util.UUID;
+import java.util.*;
 
 public class Event_JoinVerifiedCheck implements Listener {
     // 参加時にAPIを使ってVerified権限以上かどうかを調べる。違ったらdisallow
@@ -25,52 +25,82 @@ public class Event_JoinVerifiedCheck implements Listener {
         UUID uuid = event.getUniqueId();
         JSONObject json = getHttpJson("https://api.jaoafa.com/users/" + uuid);
         if (json == null) {
-            event.disallow(Result.KICK_OTHER, ChatColor.GREEN + "[jaoTest]\n\n"
-                + ChatColor.RESET + "権限の取得に失敗しました。時間をおいてからもう一回お試しください。(0)");
+            event.disallow(Result.KICK_OTHER, Component.join(
+                JoinConfiguration.noSeparators(),
+                Component.text("[jaoTest]", NamedTextColor.GREEN),
+                Component.newline(),
+                Component.newline(),
+                Component.text("権限グループの取得に失敗しました。時間をおいてからもう一回お試しください。(0)")
+            ));
             return;
         }
         if (!json.has("status") || !json.getBoolean("status")) {
-            event.disallow(Result.KICK_OTHER, ChatColor.GREEN + "[jaoTest]\n\n"
-                + ChatColor.RESET + "権限の取得に失敗しました。時間をおいてからもう一回お試しください。(1)");
+            event.disallow(Result.KICK_OTHER, Component.join(
+                JoinConfiguration.noSeparators(),
+                Component.text("[jaoTest]", NamedTextColor.GREEN),
+                Component.newline(),
+                Component.newline(),
+                Component.text("権限グループの取得に失敗しました。時間をおいてからもう一回お試しください。(1)")
+            ));
             return;
         }
         if (!json.has("data")) {
-            event.disallow(Result.KICK_OTHER, ChatColor.GREEN + "[jaoTest]\n"
-                + ChatColor.RESET + "権限の取得に失敗しました。時間をおいてからもう一回お試しください。(2)");
+            event.disallow(Result.KICK_OTHER, Component.join(
+                JoinConfiguration.noSeparators(),
+                Component.text("[jaoTest]", NamedTextColor.GREEN),
+                Component.newline(),
+                Component.newline(),
+                Component.text("権限グループの取得に失敗しました。時間をおいてからもう一回お試しください。(2)")
+            ));
             return;
         }
         JSONObject data = json.getJSONObject("data");
         if (!data.has("permission")) {
-            event.disallow(Result.KICK_OTHER, ChatColor.GREEN + "[jaoTest]\n"
-                + ChatColor.RESET + "権限の取得に失敗しました。時間をおいてからもう一回お試しください。(3)");
+            event.disallow(Result.KICK_OTHER, Component.join(
+                JoinConfiguration.noSeparators(),
+                Component.text("[jaoTest]", NamedTextColor.GREEN),
+                Component.newline(),
+                Component.newline(),
+                Component.text("権限グループの取得に失敗しました。時間をおいてからもう一回お試しください。(3)")
+            ));
             return;
         }
         String permission = data.getString("permission");
 
         Calendar startCal = Calendar.getInstance(TimeZone.getTimeZone(ZoneId.of("Asia/Tokyo")));
-        startCal.set(2021, Calendar.JUNE, 19, 0, 0, 0); // 2021/07/19 00:00:00 JST
+        startCal.set(2021, Calendar.DECEMBER, 4, 10, 0, 0); // 2021/12/04 10:00:00 JST
         Date start = startCal.getTime();
 
         Date now = new Date();
 
         Calendar endCal = Calendar.getInstance(TimeZone.getTimeZone(ZoneId.of("Asia/Tokyo")));
-        endCal.set(2021, Calendar.JUNE, 30, 23, 59, 59); // 2021/06/30 23:59:59 JST
+        endCal.set(2021, Calendar.DECEMBER, 24, 23, 59, 59); // 2021/12/24 23:59:59 JST
         Date end = endCal.getTime();
 
         if (now.before(start) &&
             !permission.equalsIgnoreCase("Admin") &&
             !permission.equalsIgnoreCase("Moderator")) {
             // 期間前 & AM以外
-            event.disallow(Result.KICK_OTHER, ChatColor.GREEN + "[jaoTest]\n"
-                + ChatColor.RESET + "イベントはまだ開始されていません。イベント開始までしばらくお待ちください。 (" + permission + ")");
+            event.disallow(Result.KICK_OTHER, Component.join(
+                JoinConfiguration.noSeparators(),
+                Component.text("[jaoTest]", NamedTextColor.GREEN),
+                Component.newline(),
+                Component.newline(),
+                Component.text("イベントはまだ開始されていません。イベント開始までしばらくお待ちください。(" + permission + ")")
+            ));
             return;
         }
         if (now.after(end) &&
             !permission.equalsIgnoreCase("Admin") &&
             !permission.equalsIgnoreCase("Moderator")) {
             // 期間後 & AM以外
-            event.disallow(Result.KICK_OTHER, ChatColor.GREEN + "[jaoTest]\n"
-                + ChatColor.RESET + "イベントは終了いたしました。ご参加ありがとうございました。 (" + permission + ")");
+            event.disallow(Result.KICK_OTHER, Component.join(
+                JoinConfiguration.noSeparators(),
+                Component.text("[jaoTest]", NamedTextColor.GREEN),
+                Component.newline(),
+                Component.newline(),
+                Component.text("イベントは終了いたしました。ご参加ありがとうございました。(" + permission + ")")
+            ));
             return;
         }
 
@@ -78,8 +108,13 @@ public class Event_JoinVerifiedCheck implements Listener {
             !permission.equalsIgnoreCase("Moderator") &&
             !permission.equalsIgnoreCase("Regular") &&
             !permission.equalsIgnoreCase("Verified")) {
-            event.disallow(Result.KICK_OTHER, ChatColor.GREEN + "[jaoTest]\n"
-                + ChatColor.RESET + "あなたにはイベントサーバに参加するための権限がありません。(" + permission + ")");
+            event.disallow(Result.KICK_OTHER, Component.join(
+                JoinConfiguration.noSeparators(),
+                Component.text("[jaoTest]", NamedTextColor.GREEN),
+                Component.newline(),
+                Component.newline(),
+                Component.text("あなたにはイベントサーバに参加するための権限がありません。(" + permission + ")")
+            ));
         }
     }
     private static JSONObject getHttpJson(String address) {
@@ -91,14 +126,14 @@ public class Event_JoinVerifiedCheck implements Listener {
                 System.out.println("[jaoTest] URLGetConnected(Error): " + address);
                 System.out.println("[jaoTest] ResponseCode: " + response.code());
                 if (response.body() != null) {
-                    System.out.println("[jaoTest] Response: " + response.body().string());
+                    System.out.println("[jaoTest] Response: " + Objects.requireNonNull(response.body()).string());
                 }
                 return null;
             }
             if (response.body() == null) {
                 return null;
             }
-            JSONObject obj = new JSONObject(response.body().string());
+            JSONObject obj = new JSONObject(Objects.requireNonNull(response.body()).string());
             response.close();
             return obj;
         } catch (IOException | JSONException e) {
