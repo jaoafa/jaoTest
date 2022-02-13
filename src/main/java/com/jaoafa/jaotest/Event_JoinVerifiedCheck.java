6f47e9fbb6d8cef6f65f76f74e1b48fe77429a9c
Tomachi [ICHIGO] <tomachi@tomacheese.com>
2022-02-13 09:46:13 +0900

    fix: 一般公開を2/19に変更

--- src/main/java/com/jaoafa/jaotest/Event_JoinVerifiedCheck.java
+++ src/main/java/com/jaoafa/jaotest/Event_JoinVerifiedCheck.java
@@ -18,11 +18,33 @@ import java.time.ZoneId;
 import java.util.*;
 
 public class Event_JoinVerifiedCheck implements Listener {
+    private static JSONObject getHttpJson(String address) {
+        try {
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder().url(address).get().build();
+            Response response = client.newCall(request).execute();
+            if (response.code() != 200) {
+                System.out.println("[jaoTest] URLGetConnected(Error): " + address);
+                System.out.println("[jaoTest] ResponseCode: " + response.code());
+                if (response.body() != null)
+                    System.out.println("[jaoTest] Response: " + Objects.requireNonNull(response.body()).string());
+                return null;
+            }
+            if (response.body() == null) return null;
+            JSONObject obj = new JSONObject(Objects.requireNonNull(response.body()).string());
+            response.close();
+            return obj;
+        } catch (IOException | JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
     // 参加時にAPIを使ってVerified権限以上かどうかを調べる。違ったらdisallow
     @EventHandler
-    public void OnLogin(AsyncPlayerPreLoginEvent event) {
+    public static void OnLogin(AsyncPlayerPreLoginEvent event) {
         UUID uuid = event.getUniqueId();
-        JSONObject json = getHttpJson("https://api.jaoafa.com/users/" + uuid);
+        JSONObject json = Event_JoinVerifiedCheck.getHttpJson("https://api.jaoafa.com/users/" + uuid);
         if (json == null) {
             event.disallow(Result.KICK_OTHER, Component.join(
                 JoinConfiguration.noSeparators(),
@@ -75,55 +97,27 @@ public class Event_JoinVerifiedCheck implements Listener {
         String permission = data.getString("permission");
 
         Calendar startCal = Calendar.getInstance(TimeZone.getTimeZone(ZoneId.of("Asia/Tokyo")));
-        if (permission.equalsIgnoreCase("Regular")) {
+        if (permission.equalsIgnoreCase("Regular"))
             startCal.set(2022, Calendar.JANUARY, 22, 17, 30, 0); // Regular - 2022/01/22 17:30:00 JST
-        } else if (permission.equalsIgnoreCase("Verified")) {
+        else if (permission.equalsIgnoreCase("Verified"))
             startCal.set(2022, Calendar.JANUARY, 26, 0, 0, 0); // Verified - 2022/01/26 00:00:00 JST
-        } else {
-            startCal.set(2022, Calendar.FEBRUARY, 28, 0, 0, 0); // Default - 2022/02/28 00:00:00 JST
-        }
+        else
+            startCal.set(2022, Calendar.FEBRUARY, 19, 0, 0, 0); // Default - 2022/02/19 00:00:00 JST
         Date start = startCal.getTime();
 
         Date now = new Date();
 
+        // 期間前 & AM以外
         if (now.before(start) &&
             !permission.equalsIgnoreCase("Admin") &&
-            !permission.equalsIgnoreCase("Moderator")) {
-            // 期間前 & AM以外
-            event.disallow(Result.KICK_OTHER, Component.join(
-                JoinConfiguration.noSeparators(),
-                Component.text("[jaoTest]", NamedTextColor.GREEN),
-                Component.newline(),
-                Component.newline(),
-                Component.text("あなたの権限グループではまだ参加できません。"),
-                Component.newline(),
-                Component.text("参加可能になるまでしばらくお待ちください。(" + permission + ")")
-            ));
-        }
-    }
-
-    private static JSONObject getHttpJson(String address) {
-        try {
-            OkHttpClient client = new OkHttpClient();
-            Request request = new Request.Builder().url(address).get().build();
-            Response response = client.newCall(request).execute();
-            if (response.code() != 200) {
-                System.out.println("[jaoTest] URLGetConnected(Error): " + address);
-                System.out.println("[jaoTest] ResponseCode: " + response.code());
-                if (response.body() != null) {
-                    System.out.println("[jaoTest] Response: " + Objects.requireNonNull(response.body()).string());
-                }
-                return null;
-            }
-            if (response.body() == null) {
-                return null;
-            }
-            JSONObject obj = new JSONObject(Objects.requireNonNull(response.body()).string());
-            response.close();
-            return obj;
-        } catch (IOException | JSONException e) {
-            e.printStackTrace();
-            return null;
-        }
+            !permission.equalsIgnoreCase("Moderator")) event.disallow(Result.KICK_OTHER, Component.join(
+            JoinConfiguration.noSeparators(),
+            Component.text("[jaoTest]", NamedTextColor.GREEN),
+            Component.newline(),
+            Component.newline(),
+            Component.text("あなたの権限グループではまだ参加できません。"),
+            Component.newline(),
+            Component.text("参加可能になるまでしばらくお待ちください。(" + permission + ")")
+        ));
     }
 }
